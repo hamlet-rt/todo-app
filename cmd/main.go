267dedaed888c1aeaff7e6e6f5155029236ff35b
Vershilo Artyom <tema.v93@gmail.com>
2024-10-11 00:27:15 +0500

    connect to db

--- cmd/main.go
+++ cmd/main.go
@@ -2,22 +2,42 @@ package main
 
 import (
 	"log"
+	"os"
 	"todo"
 	"todo/pkg/handler"
 	"todo/pkg/repository"
 	"todo/pkg/service"
 
+	_ "github.com/lib/pq"
+
 	"github.com/spf13/viper"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error initializaing configs: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error)
+	}
+
+	db, err := repository.NewPosgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
-
 
 	srv := new(todo.Server)
 	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
