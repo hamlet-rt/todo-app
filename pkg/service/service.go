1e1e97b600972ff1bdd5e70021bb84eb1faf4aa3
Vershilo Artyom <tema.v93@gmail.com>
2024-10-21 01:29:16 +0500

    add CRUD items

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -16,10 +16,15 @@ type TodoList interface {
 	GetAll(userId int) ([]todo.TodoList, error)
 	GetById(userId, listId int) (todo.TodoList, error)
 	Delete(userId, listId int) error
-	Update(userId, id int, input todo.UpdateListInput ) error
+	Update(userId, listId int, input todo.UpdateListInput ) error
 }
 
 type TodoItem interface {
+	Create(userId, listId int, item todo.TodoItem) (int, error)
+	GetAll(userId, listId int) ([]todo.TodoItem, error)
+	GetById(userId, itemId int) (todo.TodoItem, error)
+	Delete(userId, itemId int) error
+	Update(userId, itemId int, input todo.UpdateItemInput) error
 }
 
 type Service struct {
@@ -32,5 +37,6 @@ func NewService(repos *repository.Repository) *Service {
 	return &Service{
 		Authorization: NewAuthService(repos.Authorization),
 		TodoList: NewTodoListService(repos.TodoList),
+		TodoItem: NewTodoItemService(repos.TodoItem, repos.TodoList),
 	}
 }
--- pkg/service/todo_list.go
+++ pkg/service/todo_list.go
@@ -29,9 +29,9 @@ func (s *TodoListService) Delete(userId, listId int) error {
 	return s.repo.Delete(userId, listId)
 }
 
-func (s *TodoListService) Update(userId, id int, input todo.UpdateListInput ) error {
+func (s *TodoListService) Update(userId, listId int, input todo.UpdateListInput ) error {
 	if err := input.Validate(); err != nil {
 		return err
 	}
-	return s.repo.Update(userId, id, input)
+	return s.repo.Update(userId, listId, input)
 }
